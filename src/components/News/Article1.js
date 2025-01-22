8a5ab51de58076dedcb7a2e01cfff23e87ea8afb
kaiteen1003 <piasonica1003@gmail.com>
2025-01-22 20:26:45 +0900

    不要なアイコン画像の削除

--- src/components/News/Article1.js
+++ src/components/News/Article1.js
@@ -1,6 +1,5 @@
-import React from 'react';
-import './Article.css';
-//News記事を書く際にはここにコメントアウトを記すのでこれをコピペして始めるといいかも
+import React from "react";
+import "./Article.css";
 
 //変更点１　Article〇
 const Article1 = () => {
@@ -9,18 +8,15 @@ const Article1 = () => {
       <h2>2024/04/29 長井先生宅でホームパーティをしました！</h2>
       <p>おいしいごはんを食べながら歓談しました！😊</p>
 
-
       <div className="article-images">
-
         {/* 変更点2 (img src = "画像のpath")を入れる　Article〇縦写真ー＞ vertical　横写真ー＞horizontal のクラス指定をお忘れなく。以下のように記述*/}
         <div className="article-image horizontal">
           <img src="/images/Article3_1.JPG" alt="Homeparty Image 1" />
         </div>
-
       </div>
     </div>
   );
-}
+};
 
 // 変更点3 　Article〇
 export default Article1;
